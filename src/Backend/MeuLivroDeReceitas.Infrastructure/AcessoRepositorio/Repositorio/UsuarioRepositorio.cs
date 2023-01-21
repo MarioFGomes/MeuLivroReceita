3d3b080c434a5cdbd6e45fbd71e6e86fd0be6b98
Mário Gomes <marioferreiragomes333@gmial.com>
2023-01-21 13:17:09 +0100

    criar classes para acesso ao Repositorio

---
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/AcessoRepositorio/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,29 @@
+﻿using MeuLivroDeReceitas.Domain.Entidade;
+using MeuLivroDeReceitas.Domain.Repositorios;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuLivroDeReceitas.Infrastructure.AcessoRepositorio.Repositorio;
+
+public class UsuarioRepositorio : IUsuarioReadOnlyRepositorio, IUsuarioWriteOnlyRepositorio
+{
+    private readonly MeuLivroReceitaContext _contexto;
+
+    public UsuarioRepositorio(MeuLivroReceitaContext context)
+    {
+        _contexto=context;
+    }
+    public async Task Adicionar(Usuario usuario)
+    {
+        await _contexto.Usuarios.AddAsync(usuario);
+    }
+
+    public async Task<bool> ExistUsuarioEmail(string email)
+    {
+       return await _contexto.Usuarios.AnyAsync(e=>e.Email==email);
+    }
+}
--- src/Backend/MeuLivroDeReceitas.Infrastructure/Bootstrapper.cs
+++ src/Backend/MeuLivroDeReceitas.Infrastructure/Bootstrapper.cs
@@ -1,7 +1,9 @@
-﻿
-
-using FluentMigrator.Runner;
+﻿using FluentMigrator.Runner;
 using MeuLivroDeReceitas.Domain.Extension;
+using MeuLivroDeReceitas.Domain.Repositorios;
+using MeuLivroDeReceitas.Infrastructure.AcessoRepositorio;
+using MeuLivroDeReceitas.Infrastructure.AcessoRepositorio.Repositorio;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
@@ -13,6 +15,10 @@ public static class Bootstrapper
     public static void AddRespositorio(this IServiceCollection services, IConfiguration configurationManager)
     {
         AddFluentMigrator(services, configurationManager);
+        AddContexto(services, configurationManager);
+        AddRepositorios(services);
+        AddUnidadeDeTrabalho(services);
+        
     }
 
     private static void AddFluentMigrator(IServiceCollection services, IConfiguration configurationManager)
@@ -29,5 +35,31 @@ public static class Bootstrapper
         var conexao = configurationManager.GetConexa();
 
         return $"{conexao}Database={nomeDatabase}";
+    }
+
+    private static void AddRepositorios(IServiceCollection services)
+    {
+        services.AddScoped<IUsuarioReadOnlyRepositorio, UsuarioRepositorio>()
+                .AddScoped<IUsuarioWriteOnlyRepositorio, UsuarioRepositorio>();
+        
+    }
+
+    private static void AddUnidadeDeTrabalho(IServiceCollection services)
+    {
+        services.AddScoped<IUnidadeDeTrabalho, UnidateDeTrabalho>();
+                
+    }
+
+    private static void AddContexto(IServiceCollection services, IConfiguration configurationManager)
+    {
+        var versaoServidor = new MySqlServerVersion(new Version(8,0,28));
+        var conectionString = configurationManager.GetConexaoCompleta();
+
+        services.AddDbContext<MeuLivroReceitaContext>(dbContextOptions=> 
+        {
+            dbContextOptions.UseMySql(conectionString, versaoServidor);
+         
+         });
+
     }
 }
