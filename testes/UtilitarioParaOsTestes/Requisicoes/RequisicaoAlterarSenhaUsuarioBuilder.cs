fe1896985cde42dd73f8dc72434fc8e2ba115340
Mário Gomes <marioferreiragomes333@gmail.com>
2023-11-15 06:42:23 +0100

    criando teste unitario para trocar senha

---
+++ testes/UtilitarioParaOsTestes/Requisicoes/RequisicaoAlterarSenhaUsuarioBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+using MeuLivroDeReceitas.Comunicacao.Requisicoes;
+using MeuLivroDeReceitas.Exceptions;
+
+
+namespace UtilitarioParaOsTestes.Requisicoes;
+
+public class RequisicaoAlterarSenhaUsuarioBuilder
+{
+    public static RequisicaoAlterarSenha Construir(int tamanhosenha=10)
+    {
+
+        return new Faker<RequisicaoAlterarSenha>()
+            .RuleFor(c => c.SenhaAntiga, f => f.Internet.Password(10))
+            .RuleFor(c => c.NovaSenha, f => f.Internet.Password(tamanhosenha));
+    }
+}
